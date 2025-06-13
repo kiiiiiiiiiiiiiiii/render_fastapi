49e28110913b2ecf7c72eae4684397c7a6e98751
kiiiiiiiiiiiiiiii <kiichi20040428@gmail.com>
2025-06-13 11:44:59 +0900

    ✨ index, hellowの追加

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import datetime
 from typing import Optional
 
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+import random  # randomライブラリを追加
 
 app = FastAPI()
 
@@ -9,6 +13,42 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/index")
+def index():
+    html_content = """
+        <html>
+            <head>
+                <title>Some HTML in here</title>
+            </head>
+            <body>
+                <h1>Look ma! HTML!</h1>
+                <p>こんにちは！</p>
+            </body>
+        </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/omikuji")
+def omikuji():
+    omikuji_list = [
+        "大吉",
+        "中吉",
+        "小吉",
+        "吉",
+        "半吉",
+        "末吉",
+        "末小吉",
+        "凶",
+        "小凶",
+        "大凶"
+    ]
+
+    return {"result": omikuji_list[random.randrange(10)]}
+
+@app.post("/hello")
+async def reply(name):
+    return {"response": f"こんにちは！{name}! 今{datetime.datetime.now()}だよ！また会おうね！"} 
